08f7a921b9e56bdde19c6a6924c724082c00c75a
Amnesio <74130581+Amnesio@users.noreply.github.com>
2022-09-07 12:49:56 +0900

    雑魚敵LesserEnemy　作成 Playerを追いかける移動のみ

---
+++ ButiEngine_User/LesserEnemy.cpp
@@ -0,0 +1,98 @@
+#include "stdafx_u.h"
+#include "LesserEnemy.h"
+#include "InputManager.h"
+#include "Header/GameObjects/DefaultGameComponent/RigidBodyComponent.h"
+#include "SeparateDrawObject.h"
+#include "Gun.h"
+#include "Player.h"
+
+void ButiEngine::LesserEnemy::OnUpdate()
+{
+	if (m_vlp_velocityDicisionInterval->Update())
+	{
+		DecideVelocity();
+	}
+	Move();
+}
+
+void ButiEngine::LesserEnemy::OnSet()
+{
+}
+
+void ButiEngine::LesserEnemy::Start()
+{
+	m_vwp_rigidBody = gameObject.lock()->GetGameComponent<RigidBodyComponent>();
+	m_vwp_drawObjectTransform = gameObject.lock()->GetGameComponent<SeparateDrawObject>()->GetDrawObject().lock()->transform;
+	m_vwp_player = GetManager().lock()->GetGameObject("Player");
+	m_vlp_velocityDicisionInterval = ObjectFactory::Create<RelativeTimer>(m_moveRate);
+	m_vlp_velocityDicisionInterval->Start();
+	m_velocity = Vector3();
+	DecideVelocity();
+}
+
+void ButiEngine::LesserEnemy::OnRemove()
+{
+}
+
+void ButiEngine::LesserEnemy::OnShowUI()
+{
+	GUI::BulletText(u8"ˆÚ“®‘¬“x");
+	GUI::DragFloat("##speed", &m_speed, 1.0f, 0.0f, 10.0f);
+
+	GUI::BulletText(u8"ˆÚ“®•ûŒüŒˆ’èŠÔŠu");
+	if (GUI::DragInt("##moveRate", &m_moveRate, 1.0f, 1.0f, 60.0f))
+	{
+		if (m_vlp_velocityDicisionInterval)
+		{
+			m_vlp_velocityDicisionInterval->ChangeCountFrame(m_moveRate);
+		}
+	}
+}
+
+ButiEngine::Value_ptr<ButiEngine::GameComponent> ButiEngine::LesserEnemy::Clone()
+{
+	return ObjectFactory::Create<LesserEnemy>();
+}
+
+void ButiEngine::LesserEnemy::Dead()
+{
+	gameObject.lock()->GetGameComponent<SeparateDrawObject>()->Dead();
+	gameObject.lock()->SetIsRemove(true);
+}
+
+void ButiEngine::LesserEnemy::Move()
+{
+	//Player‚É‹ß‚Ã‚«‚½‚¢
+	/*Vector2 rightStick = InputManager::GetRightStick();
+	Vector3 velocity = Vector3(rightStick.x, 0.0f, rightStick.y).Normalize() * speed;*/
+	m_vwp_rigidBody.lock()->GetRigidBody()->SetVelocity(m_velocity * m_speed * GameDevice::WorldSpeed);
+}
+
+void ButiEngine::LesserEnemy::DecideVelocity()
+{
+	m_vlp_velocityDicisionInterval->Reset();
+
+	//‹——£ŒvŽZ
+	Value_weak_ptr<RigidBodyComponent> playerRigidBody = m_vwp_player.lock()->GetGameComponent<RigidBodyComponent>();
+	Vector3 playerPosition = playerRigidBody.lock()->GetRigidBody()->GetPosition();
+	Vector3 position = m_vwp_rigidBody.lock()->GetRigidBody()->GetPosition();
+	float distance = playerPosition.Distance(position);
+	
+	float minimumDistance = 3.0f;
+	if (distance <= minimumDistance)
+	{
+		//ƒ‰ƒ“ƒ_ƒ€‚É‚µ‚½‚¢
+	}
+	else
+	{
+		m_velocity = Vector3(playerPosition.x - position.x, 0, playerPosition.z - position.z);
+	}
+}
+
+void ButiEngine::LesserEnemy::Shoot()
+{
+}
+
+void ButiEngine::LesserEnemy::CreateGun()
+{
+}
---
+++ ButiEngine_User/LesserEnemy.h
@@ -0,0 +1,59 @@
+#pragma once
+#include"Header/GameComponentHeader.h"
+namespace ButiEngine {
+
+	class RigidBodyComponent;
+	class Gun;
+
+	class LesserEnemy :public GameComponent
+	{
+	public:
+
+		std::string GetGameComponentName()const override {
+			return "LesserEnemy";
+		}
+		void OnUpdate()override;
+		void OnSet()override;
+		void Start()override;
+		void OnRemove()override;
+		void OnShowUI()override;
+		Value_ptr<GameComponent> Clone()override;
+		template<class Archive>
+		void serialize(Archive& archive)
+		{
+			archive(isActive);
+			archive(m_speed);
+			archive(m_moveRate);
+		}
+
+		void Dead();
+	private:
+		void Move();
+		void DecideVelocity();
+		void Shoot();
+
+		void CreateGun();
+
+		//物理挙動関連
+		Value_weak_ptr<RigidBodyComponent> m_vwp_rigidBody;
+
+		//描画用オブジェクト関連
+		Value_weak_ptr<Transform> m_vwp_drawObjectTransform;
+
+		//銃関連
+		Value_weak_ptr<GameObject> m_vwp_gun;
+		Value_weak_ptr<Gun> m_vwp_gunComponent;
+
+		//Player視認用
+		Value_weak_ptr<GameObject> m_vwp_player;
+
+		//移動
+		Value_ptr<RelativeTimer> m_vlp_velocityDicisionInterval;
+		std::int32_t m_moveRate = 10;
+		float m_speed = 1.0f;
+		Vector3 m_velocity;
+	};
+
+}
+
+BUTI_REGIST_GAMECOMPONENT(LesserEnemy, true);

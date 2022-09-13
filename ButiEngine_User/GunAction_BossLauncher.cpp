6aef4d625d63425e49516d701c09003bb4b99dcd
Amnesio <74130581+Amnesio@users.noreply.github.com>
2022-09-14 06:45:02 +0900

    GunAction_BossLauncher　途中

--- ButiEngine_User/GunAction_BossLauncher.cpp
+++ ButiEngine_User/GunAction_BossLauncher.cpp
@@ -1,2 +1,122 @@
 #include "stdafx_u.h"
 #include "GunAction_BossLauncher.h"
+#include "EquipGun.h"
+#include "Gun.h"
+#include "SeparateDrawObject.h"
+#include "BossEnemy.h"
+#include "Header/GameObjects/DefaultGameComponent/RigidBodyComponent.h"
+#include "Header/GameObjects/DefaultGameComponent/PositionAnimationComponent.h"
+#include "Header/GameObjects/DefaultGameComponent/RotationAnimationComponent.h"
+
+void ButiEngine::GunAction_BossLauncher::OnUpdate()
+{
+	if (m_vwp_bossEnemyComponent.lock()->IsPause())
+	{
+		return;
+	}
+
+	switch (m_phase)
+	{
+	case ButiEngine::GunAction_BossLauncherPhase::LockOn:
+		break;
+	case ButiEngine::GunAction_BossLauncherPhase::Shoot:
+		break;
+	case ButiEngine::GunAction_BossLauncherPhase::Rest:
+		break;
+	case ButiEngine::GunAction_BossLauncherPhase::AfterShoot:
+		break;
+	default:
+		break;
+	}
+}
+
+void ButiEngine::GunAction_BossLauncher::OnSet()
+{
+	m_vwp_drawObject = gameObject.lock()->GetGameComponent<SeparateDrawObject>()->GetDrawObject();
+	//m_vwp_drawObject.lock()->GetGameComponent<LookAtComponent>()->SetIsActive(false);
+
+	m_vwp_bossEnemyComponent = gameObject.lock()->GetGameComponent<BossEnemy>();
+	m_vwp_bossEnemyComponent.lock()->StartGunAction();
+
+	m_vwp_gunComponent = m_vwp_bossEnemyComponent.lock()->ChangeGun("Gun_Boss_Launcher");
+
+	m_lockOnTime = 100;
+	m_restTime = 30;
+	m_afterTime = 30;
+	m_actionTime = 10;
+	m_vlp_waitTimer = ObjectFactory::Create<RelativeTimer>(m_lockOnTime);
+	m_vlp_actionTimer = ObjectFactory::Create<RelativeTimer>(m_actionTime);
+	m_shootCount = 0;
+	m_shootCountMax = 3;
+}
+
+void ButiEngine::GunAction_BossLauncher::Start()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::OnRemove()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::OnShowUI()
+{
+	GUI::BulletText(u8"waitTimer");
+	GUI::Text(m_vlp_waitTimer->GetCurrentCountFrame());
+	GUI::BulletText(u8"actionTimer");
+	GUI::Text(m_vlp_actionTimer->GetCurrentCountFrame());
+	GUI::BulletText(u8"shootCount");
+	GUI::Text(m_shootCount);
+}
+
+ButiEngine::Value_ptr<ButiEngine::GameComponent> ButiEngine::GunAction_BossLauncher::Clone()
+{
+	return ObjectFactory::Create<GunAction_BossLauncher>();
+}
+
+void ButiEngine::GunAction_BossLauncher::StartPause()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::FinishPause()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::StartLockOnPhase()
+{
+	m_phase = GunAction_BossLauncherPhase::LockOn;
+}
+
+void ButiEngine::GunAction_BossLauncher::UpdateLockOnPhase()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::StartShootPhase()
+{
+	m_phase = GunAction_BossLauncherPhase::Shoot;
+}
+
+void ButiEngine::GunAction_BossLauncher::UpdateShootPhase()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::StartRestPhase()
+{
+	m_phase = GunAction_BossLauncherPhase::Rest;
+}
+
+void ButiEngine::GunAction_BossLauncher::UpdateRestPhase()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::StartAfterShootPhase()
+{
+	m_phase = GunAction_BossLauncherPhase::AfterShoot;
+}
+
+void ButiEngine::GunAction_BossLauncher::UpdateAfterShootPhase()
+{
+}
+
+void ButiEngine::GunAction_BossLauncher::Rotate()
+{
+}
--- ButiEngine_User/GunAction_BossLauncher.h
+++ ButiEngine_User/GunAction_BossLauncher.h
@@ -1,5 +1,66 @@
 #pragma once
-class GunAction_BossLauncher
-{
-};
+#include"Header/GameComponentHeader.h"
+namespace ButiEngine {
 
+	class BossEnemy;
+	class Gun;
+
+	enum class GunAction_BossLauncherPhase
+	{
+		LockOn,
+		Shoot,
+		Rest,
+		AfterShoot
+	};
+
+	class GunAction_BossLauncher :public GameComponent
+	{
+	public:
+		std::string GetGameComponentName()const override {
+			return "GunAction_BossLauncher";
+		}
+		void OnUpdate()override;
+		void OnSet()override;
+		void Start()override;
+		void OnRemove()override;
+		void OnShowUI()override;
+		Value_ptr<GameComponent> Clone()override;
+		template<class Archive>
+		void serialize(Archive& archive)
+		{
+			archive(isActive);
+		}
+
+		void StartPause();
+		void FinishPause();
+	private:
+		void StartLockOnPhase();
+		void UpdateLockOnPhase();
+		void StartShootPhase();
+		void UpdateShootPhase();
+		void StartRestPhase();
+		void UpdateRestPhase();
+		void StartAfterShootPhase();
+		void UpdateAfterShootPhase();
+		void Rotate();
+
+		GunAction_BossLauncherPhase m_phase;
+
+		Value_ptr<RelativeTimer> m_vlp_waitTimer;
+		std::int32_t m_lockOnTime;
+		std::int32_t m_restTime;
+		std::int32_t m_afterTime;
+		Value_ptr<RelativeTimer> m_vlp_actionTimer;
+		std::int32_t m_actionTime;
+		Value_ptr<Transform> m_vwp_startTransform;
+		std::int32_t m_shootCount;
+		std::int32_t m_shootCountMax;
+
+		Value_weak_ptr<BossEnemy> m_vwp_bossEnemyComponent;
+		Value_weak_ptr<Gun> m_vwp_gunComponent;
+		Value_weak_ptr<GameObject> m_vwp_drawObject;
+	};
+
+}
+
+BUTI_REGIST_GAMECOMPONENT(GunAction_BossLauncher, true);
